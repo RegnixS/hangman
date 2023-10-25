d94ca4a12a66021dbff6e428c3d477f11c82fa6a
RegnixS <rob.ducke@gmail.com>
2023-10-25 14:20:58 +0100

    add milestone_2.py

---
+++ hangman/milestone_3.py
@@ -0,0 +1,29 @@
+# %%
+import random
+
+def check_guess(guess):
+    guess = guess.lower()
+    if guess in word:
+        print(f'Good guess! {guess} is in the word.')
+    else:
+        print(f'Sorry, {guess} is not in the word. Try again.')
+
+word_list = ['banana', 'mango', 'kiwi', 'plum', 'pear']
+print(word_list)
+word = random.choice(word_list)
+print(word)
+
+def ask_for_input():
+    while True:
+        guess = input('Please enter a letter ')
+        print(guess)
+
+        if len(guess) == 1 and guess.isalpha():
+            break
+        else:
+            print('Invalid letter. Please, enter a single alphabetical character.') 
+
+    check_guess(guess)
+
+ask_for_input()
+# %%   
